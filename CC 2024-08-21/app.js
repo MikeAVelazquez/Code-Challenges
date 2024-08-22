c6b2b4b66dc0c20002fb73525e2fdd0d06aad9d9
mvelazquez808 <76184519+mvelazquez808@users.noreply.github.com>
2024-08-21 19:53:32 -1000

    Challlenge Solved

--- CC 2024-08-21/app.js
+++ CC 2024-08-21/app.js
@@ -1,7 +1,19 @@
 // Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
-// Examples
+
+
+// Par: 
+// R: Return a string with each word reversed. keep words in original order
+// E:
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
+// P: make a function that splits up the string into individual strings, reverse each string and add to new string
 
+
+
+function reverseWords(str) {
+  
+  //split words into seperate arrays
+  return str.split("").reverse().join("").split(" ").reverse().join(" ");
+}
 
